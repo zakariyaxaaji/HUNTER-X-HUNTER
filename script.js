c2b163d0fb7f0e70bebbf79e308b9bd13024de6f
zakariyaxaaji <zakariyaxaaji@gmail.com>
2024-02-11 20:55:19 +0100

    search finally works 🎉✨🎊

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // const personalInfo = {
-//   email: "zakariyaxaaji@gmail.com",
-//   name: "Zakariya Afrah",
+//   email: "yourEmail@gmail.com",
+//   name: "your name",
 //   usage: "am trying to learn how to use APIs",
 // };
 // fetching to register sooo i can get API key
@@ -14,61 +14,73 @@
 //   .then((res) => res.json())
 //   .then((data) => console.log(data));
 
-const baseURL = "https://hxh-api.onrender.com"; // should this end with a trailing slash (/)
+//variables
+const baseURL = "https://hxh-api.onrender.com";
 const api_key =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YzBmZGI1MDYyMDQxMWZjYjNmODM3OCIsImlhdCI6MTcwNzE0NjY3NywiZXhwIjoxNzA5NzM4Njc3fQ.xlIScOWTIR2i2MkWGufclmkmsncaEBTJaxoAit2MH7g";
+let ArrayOfCharacters = []; // this is populated from fetchData()
+const cards = document.querySelector(".cards");
+const temp = document.querySelector(".template");
+const content = temp.content;
+const searchInput = document.querySelector("#search");
 
+// Create helper funcitons below
 async function fetchData(baseURL, api_key) {
-  // async function returns a promise
   const response = await fetch(
     `${baseURL}/api/v1/characters?api_key=${api_key}&page=1&limit=24`
   );
   const data = await response.json(); //data is an object
   const results = data.data; // results is an Array of objects
+  // console.log(results);
 
-  const ArrayOfCharacters = results.map((result) => {
-    const character = {
-      name: "",
-      gender: "",
-      also_known_as: undefined,
-      nen_type: undefined,
-      img: "",
+  ArrayOfCharacters = results.map((result) => {
+    return {
+      name: result.name,
+      gender: result.gender,
+      also_known_as: result.also_known_as,
+      nen_type: result.nen_type,
+      img: result.image[0].secure_url,
     };
-    character.name = result.name;
-    character.gender = result.gender;
-    character.also_known_as = result.also_known_as;
-    character.nen_type = result.nen_type;
-    character.img = result.image[0].secure_url;
-    return character;
   });
-  createCard(ArrayOfCharacters);
-  return ArrayOfCharacters;
 }
-fetchData(baseURL, api_key);
 
-const cards = document.querySelector(".cards");
 function createCard(array) {
-  const elements = [];
-  const htmlString = array
-    .map((arr, index) => {
-      return `<li class="card">
-      <img
-        src="${arr.img}"
-        alt="image of ${arr.name} "
-      />
-      <h2 class ="name">${index + 1}. ${arr.name}</h2>
-      <p>
-        Gender: ${arr.gender} <br />
-        Nen-typ: ${arr.nen_type} <br />
-      </p>
-    </li>`;
-    })
-    .join(" ");
+  array.forEach((element, index) => {
+    // Clone the template content for each item
+    const clone = content.cloneNode(true);
 
-  cards.innerHTML = htmlString;
+    // Modify the cloned content with data from the array item
+    let el = clone.querySelector(".card");
+    const name = clone.querySelector(".name");
+    const img = clone.querySelector(".img");
+    const gender = clone.querySelector(".gender");
+    const nen = clone.querySelector(".nen");
+
+    element["li"] = el;
+    name.textContent = `${index + 1}  ${element.name}`;
+    gender.textContent = `Gender: ${element.gender}`;
+    nen.textContent = `Nen-type: ${element.name}`;
+    img.setAttribute("src", element.img);
+    img.setAttribute("alt", `this is an img of ${element.name}`);
+
+    // Append the modified content to the cards
+    cards.appendChild(clone);
+  });
 }
 
-const searchBar = document.querySelector("#search");
-const searchGlass = document.querySelector(".search>img");
+//Search functionality
+searchInput.addEventListener("input", (e) => {
+  const text = e.target.value.toLowerCase();
+  ArrayOfCharacters.forEach((character) => {
+    const isVisible =
+      character.name.toLowerCase().includes(text) ||
+      character.nen_type.includes(text);
+    character.li.classList.toggle("hide", !isVisible);
+  });
+});
 
-// the logic for a searchbar
+// Run functions below
+(async () => {
+  await fetchData(baseURL, api_key);
+  createCard(ArrayOfCharacters);
+})();
